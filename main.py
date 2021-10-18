0bf3366007207114ad22ffd26af468937db12f32
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-10-18 19:09:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+targil 1:
+  
+def printnums():
+    for i in range(1,101):
+        print(i)
+        
+targil 2:
+  
+def prints():
+  x = 10
+  for i in range(0,x+1):
+      print(i)
+        
+targil 3:
+  
+import random
+
+def luckynum():
+    lucky = random.randint(1,11)
+    tries = 0
+    while True:
+        num = int(input("please pick a number: "))
+        tries += 1
+        if num < lucky:
+            print("lower number")
+        elif num > lucky:
+            print("higher number")
+        else:
+            print("bingo")
+            break
+    print("it took: ",tries, "guesses to user to guess")
+  
+  
